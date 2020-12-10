26bf8c6f692bfd03fdfe8abc841b77f47d9d7f42
Brad Cowie <brad@wand.net.nz>
2020-12-10 14:59:54 +1300

    try this

--- rdma-core/boot.sh
+++ rdma-core/boot.sh
@@ -1,5 +1,1 @@
-set -x
-
-git clone --recurse-submodules https://github.com/linux-rdma/rdma-core -b v32.0
-cd rdma-core
-pwd
+git clone --recurse-submodules https://github.com/linux-rdma/rdma-core -b v32.0 src
