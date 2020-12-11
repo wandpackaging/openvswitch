b9395d1bf85168c0ac1c0e854d5a60bcc631d8ce
Brad Cowie <brad@wand.net.nz>
2020-12-11 14:54:03 +1300

    build changes to support older linuxes

--- libbpf/boot.sh
+++ libbpf/boot.sh
@@ -1,4 +1,12 @@
 git clone --recurse-submodules https://github.com/sudipm-mukherjee/libbpf -b debian/0.2-1 src
-env
-sed -i 's/debhelper-compat (= 13)/debhelper (>= 10)/' src/debian/control
-echo "10" > src/debian/compat
+
+if ${BUILD_TARGET} == "ubuntu:xenial"; then
+    sed -i 's/DEB_BUILD_MAINT_OPTIONS = hardening=+all/DEB_BUILD_MAINT_OPTIONS =/' src/debian/rules
+fi
+
+debhelper_version=$(dpkg-query --showformat='${Version}' --show debhelper)
+
+if dpkg --compare-versions "${debhelper_version}" lt 13; then
+    sed -i 's/debhelper-compat (= 13)/debhelper (>= 10)/' src/debian/control
+    echo "10" > src/debian/compat
+fi
