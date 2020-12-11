ebe99bcd3557d9476c7390786aadc0a05894ef7b
Brad Cowie <brad@wand.net.nz>
2020-12-11 16:50:41 +1300

    older version

--- nasm/boot.sh
+++ nasm/boot.sh
@@ -2,12 +2,12 @@
 
 set -euo pipefail
 
-git clone --recurse-submodules https://salsa.debian.org/debian/nasm -b debian/2.15.05-1 src
+git clone --recurse-submodules https://salsa.debian.org/debian/nasm -b debian/2.14.02-1 src
 
 debhelper_version=$(dpkg-query --showformat='${Version}' --show debhelper)
 
-if dpkg --compare-versions "${debhelper_version}" lt 13; then
+if dpkg --compare-versions "${debhelper_version}" lt 12; then
     sed -i 's/fonts-liberation2/fonts-liberation/' src/debian/control
-    sed -i 's/debhelper-compat (= 13)/debhelper (>= 10)/' src/debian/control
+    sed -i 's/debhelper (>= 12)/debhelper (>= 10)/' src/debian/control
     echo "10" > src/debian/compat
 fi
