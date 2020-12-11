749d71a766b8f00f6cee41302e3fa41692aa8e4d
Brad Cowie <brad@wand.net.nz>
2020-12-11 16:44:19 +1300

    fonts..

--- nasm/boot.sh
+++ nasm/boot.sh
@@ -7,6 +7,7 @@ git clone --recurse-submodules https://salsa.debian.org/debian/nasm -b debian/2.
 debhelper_version=$(dpkg-query --showformat='${Version}' --show debhelper)
 
 if dpkg --compare-versions "${debhelper_version}" lt 13; then
+    sed -i 's/fonts-liberation2/fonts-liberation/' src/debian/control
     sed -i 's/debhelper-compat (= 13)/debhelper (>= 10)/' src/debian/control
     echo "10" > src/debian/compat
 fi
