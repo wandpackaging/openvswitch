52585ee84da772d80aacf4811c24589fd4b3b77b
Brad Cowie <brad@wand.net.nz>
2020-12-11 14:36:10 +1300

    debug

--- libbpf/boot.sh
+++ libbpf/boot.sh
@@ -1,3 +1,4 @@
 git clone --recurse-submodules https://github.com/sudipm-mukherjee/libbpf -b debian/0.2-1 src
+env
 sed -i 's/debhelper-compat (= 13)/debhelper (>= 10)/' src/debian/control
 echo "10" > src/debian/compat
