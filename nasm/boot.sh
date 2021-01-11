eaba533bcff1a7ca183c946db993178c6821481d
Brad Cowie <brad@wand.net.nz>
2021-01-11 14:25:35 +1300

    fix nasm builds

--- nasm/boot.sh
+++ nasm/boot.sh
@@ -4,10 +4,16 @@ set -euo pipefail
 
 git clone --recurse-submodules https://salsa.debian.org/debian/nasm -b debian/2.14.02-1 src
 
+sed -i 's/$(MAKE) doc/$(MAKE) doc manpages/' src/debian/rules
+
 debhelper_version=$(dpkg-query --showformat='${Version}' --show debhelper)
 
 if dpkg --compare-versions "${debhelper_version}" lt 12; then
     sed -i 's/fonts-liberation2/fonts-liberation/' src/debian/control
     sed -i 's/debhelper (>= 12)/debhelper (>= 10)/' src/debian/control
     echo "10" > src/debian/compat
+fi
+
+if ! dpkg-query -W fonts-liberation2; then
+    sed -i 's/fonts-liberation2/fonts-liberation/' src/debian/control
 fi
