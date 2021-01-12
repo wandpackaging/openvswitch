cfd0a37a6a20745700619ca3be4ccffaf1a516af
Brad Cowie <brad@wand.net.nz>
2021-01-12 14:15:04 +1300

    only need to build libisal on xenial

--- libisal/boot.sh
+++ libisal/boot.sh
@@ -2,11 +2,4 @@
 
 set -euo pipefail
 
-git clone --recurse-submodules https://salsa.debian.org/openstack-team/third-party/libisal.git -b debian/2.29.0-2 src
-
-debhelper_version=$(dpkg-query --showformat='${Version}' --show debhelper)
-
-if dpkg --compare-versions "${debhelper_version}" lt 12; then
-    sed -i 's/debhelper-compat (= 12)/debhelper (>= 10)/' src/debian/control
-    echo "10" > src/debian/compat
-fi
+git clone --recurse-submodules https://salsa.debian.org/openstack-team/third-party/libisal.git -b debian/2.20.0-1 src
