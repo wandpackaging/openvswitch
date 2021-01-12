fd795bda695a16f9d91c3a479da53779d519d31a
Brad Cowie <brad@wand.net.nz>
2021-01-13 11:39:12 +1300

    disable hardening on xenial

--- dpdk/boot.sh
+++ dpdk/boot.sh
@@ -3,3 +3,7 @@
 set -euo pipefail
 
 git clone --recurse-submodules https://salsa.debian.org/debian/dpdk.git -b debian/19.11.6-1 src
+
+if [ "${BUILD_TARGET}" == "ubuntu:xenial" ]; then
+    sed -i 's/DEB_BUILD_MAINT_OPTIONS = hardening=+all/DEB_BUILD_MAINT_OPTIONS =/' src/debian/rules
+fi
