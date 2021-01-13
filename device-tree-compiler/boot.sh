92a87447aab4bfa343497197f8ad0c1b620711f6
Brad Cowie <brad@wand.net.nz>
2021-01-14 11:37:36 +1300

    build newer device-tree-compiler

--- device-tree-compiler/boot.sh
+++ device-tree-compiler/boot.sh
@@ -2,4 +2,4 @@
 
 set -euo pipefail
 
-git clone --recurse-submodules https://salsa.debian.org/crosstoolchain-team/device-tree-compiler -b debian/1.4.2-1 src
+git clone --recurse-submodules https://salsa.debian.org/crosstoolchain-team/device-tree-compiler -b debian/1.4.4-1 src
