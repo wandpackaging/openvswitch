e55f343a90856b31b97167c55d4dc6374e269875
Brad Cowie <brad@wand.net.nz>
2021-01-13 15:11:39 +1300

    don't use debhelper-compat

--- nasm/boot.sh
+++ nasm/boot.sh
@@ -8,6 +8,13 @@ apt-get install -y asciidoc
 
 sed -i 's/$(MAKE) doc/$(MAKE) doc manpages/' src/debian/rules
 
+debhelper_version=$(dpkg-query --showformat='${Version}' --show debhelper)
+
+if dpkg --compare-versions "${debhelper_version}" lt 12; then
+    sed -i 's/debhelper (>= 12)/debhelper (>= 11)/' src/debian/control
+    echo "11" > src/debian/compat
+fi
+
 if ! dpkg-query -W fonts-liberation2; then
     sed -i 's/fonts-liberation2/fonts-liberation/' src/debian/control
 fi
