d969ed78e110cb651f5cbb940211157f6f6ad27f
Brad Cowie <brad@wand.net.nz>
2021-01-18 16:50:03 +1300

    debhelper 11

--- dpdk/boot.sh
+++ dpdk/boot.sh
@@ -7,3 +7,10 @@ git clone --recurse-submodules https://salsa.debian.org/debian/dpdk.git -b debia
 if [ "${BUILD_TARGET}" == "ubuntu:xenial" ]; then
     sed -i 's/DEB_BUILD_MAINT_OPTIONS = hardening=+all/DEB_BUILD_MAINT_OPTIONS =/' src/debian/rules
 fi
+
+debhelper_version=$(dpkg-query --showformat='${Version}' --show debhelper)
+
+if dpkg --compare-versions "${debhelper_version}" lt 12; then
+    sed -i 's/debhelper-compat (= 12)/debhelper (>= 11)/' src/debian/control
+    echo '11' > src/debian/compat
+fi
