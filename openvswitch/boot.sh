c871b8674fa1316ae168694a91edc9b2a5625e62
Brad Cowie <brad@wand.net.nz>
2021-01-18 22:18:11 +1300

    debhelper 11

--- openvswitch/boot.sh
+++ openvswitch/boot.sh
@@ -6,3 +6,11 @@ git clone --recurse-submodules https://salsa.debian.org/openstack-team/third-par
 
 # ovs won't build from a git checkout
 rm -rf ./src/.git
+
+debhelper_version=$(dpkg-query --showformat='${Version}' --show debhelper)
+
+if dpkg --compare-versions "${debhelper_version}" lt 12; then
+    sed -i 's/debhelper-compat (= 12)/debhelper (>= 11)/' src/debian/control
+    echo '11' > src/debian/compat
+fi
+
