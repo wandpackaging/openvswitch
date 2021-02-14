e7d96f510bc94ad5a29022e28951e2f5e4abbcb1
Brad Cowie <brad@wand.net.nz>
2021-02-15 09:56:43 +1300

    Use latest 2.15.0 tag.

--- openvswitch/boot.sh
+++ openvswitch/boot.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-git clone --recurse-submodules https://salsa.debian.org/openstack-team/third-party/openvswitch -b debian/2.15.0_git20210104.def6eb1ea+dfsg1-4 src
+git clone --recurse-submodules https://salsa.debian.org/openstack-team/third-party/openvswitch -b debian/2.15.0_git20210104.def6eb1ea+dfsg1-5 src
 
 # ovs won't build from a git checkout
 rm -rf ./src/.git
